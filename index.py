48d56daf0d52336e6b79bb2387a6f4611c786c4a
bartoszkruba <bartosz.kruba@gmail.com>
2019-11-27 13:11:13 +0100

    dd

--- index.py
+++ index.py
@@ -1,15 +1,62 @@
 import gym
 import numpy as np
 
-env = gym.make("Pendulum-v0")
+env = gym.make("MountainCar-v0")
 
-done = False
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 10000
 
-env.reset()
+epsilon = 0.5
+START_EPSILON_DECAYING = 0
+END_EPSILON_DECAYING = EPISODES // 3
+EPSILON_DECAY_STEP = epsilon / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
-while not done:
-    env.render()
-    action = 0
-    new_state, reward, done, _ = env.step(env.action_space.sample())
+currentEpisode = 0
+
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+
+DISCRETE_OS_WIN_SIZE = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+
+q_table = np.random.uniform(low=-2, high=0, size=DISCRETE_OS_SIZE + [env.action_space.n])
+
+
+def get_discrete_state(state):
+    disc_state = (state - env.observation_space.low) / DISCRETE_OS_WIN_SIZE
+    return tuple(disc_state.astype(np.int))
+
+
+for episode in range(EPISODES):
+
+    done = False
+    state = env.reset()
+
+    while not done:
+        if episode % 500 == 0:
+            env.render()
+
+        if episode >= START_EPSILON_DECAYING:
+            epsilon -= EPSILON_DECAY_STEP
+
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[get_discrete_state(state)])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+
+        new_state, reward, done, _ = env.step(action)
+
+        if not done:
+            current_q = np.max(q_table[get_discrete_state(state)])
+            future_q = np.max(q_table[get_discrete_state(new_state)])
+
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * future_q)
+
+            q_table[get_discrete_state(state) + (action,)] = new_q
+        elif new_state[0] >= env.goal_position:
+            q_table[get_discrete_state(state) + (action,)] = 0
+
+        state = new_state
+
+    currentEpisode = currentEpisode + 1
 
 env.close()
